5c6c3a47de6c4d5588072c222a0a4e0af8f92bf5
Max Dawson <maxjdawson7@gmail.com>
2024-04-19 18:03:59 +0100

    The copyright all works now on all pages. (maybe not login)

--- pages.js
+++ pages.js
@@ -1,61 +1,49 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    changeFooter();
-});
-
-window.addEventListener('popstate', function(event) {
-    console.log('here');
-    changeFooter();
-});
-  
-
-function changeFooter(){
-    if(typeof onAdminPage !== 'undefined' && onAdminPage){
+if(typeof onAdminPage !== 'undefined' && onAdminPage){
+    waitForElement('footer.main-footer',function(){
         const footer = document.querySelector('.main-footer');
-
+    
         // Remove the old copright - for some reason pterodactyl does it really strangly and it's hard to edit
         footer.childNodes[2].remove();  // Remove with ID 2
         footer.childNodes[2].remove();  // Remove the new node with ID 2 - but would be 3rd
         footer.childNodes[2].remove();  // Remove the new node with ID 2 - but would be 4th
-
+    
         // Update the text
         footer.innerHTML += copyrightText;
-    } else{
-        waitForElement('.PageContentBlock___StyledP-sc-kbxq2g-3.dcHyfd', function(element) {
-            const footer = document.querySelector('.PageContentBlock___StyledP-sc-kbxq2g-3.dcHyfd');
-            footer.innerHTML = copyrightText;    // Not += because we want to completely remove what's already there
-        });
+    });
+} else{
+    function changeFooter(observer){
+        observer.disconnect();
+        const footer = document.querySelector('[class^="ContentContainer-"][class*=" PageContentBlock___StyledContentContainer"] [class^="PageContentBlock___Styled"]');
+        footer.innerHTML = copyrightText;    // Not += because we want to completely remove what's already there
+        observer.observe(document.documentElement,{childList:true,subtree:true});
     }
-}
-
-{
-    var i = 0;
-    document.querySelector('.fade-enter-done').children[0].children.forEach((child), ()=>{
-        if(i != 0){
-            child.addEventListener('click', function(){
-                changeFooter();
-            })
-        }
-        i++;
-    })
+    
+    const observer = new MutationObserver((mutationsList) => {
+        mutationsList.forEach((mutation) => {
+          if (mutation.addedNodes.length) {
+            mutation.addedNodes.forEach((node) => {
+                if (node.nodeType === 1 && document.querySelector('#app').contains(node.parentNode)) {
+                    if(document.querySelector('[class^="ContentContainer-"][class*=" PageContentBlock___StyledContentContainer"] [class^="PageContentBlock___Styled"] a[class^="PageContentBlock___Styled"]')){
+                        console.log('Change');
+                        changeFooter(observer);
+                    }
+                }
+            });
+          }
+        });
+    });
+    
+    waitForElement('#app',function(){observer.observe(document.documentElement,{childList:true,subtree:true});});
 }
 
 // Function to wait for an element to appear
 function waitForElement(selector, callback) {
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            // Check if the target node or its subtree contains the desired element
-            if (document.querySelector(selector)) {
-                // Disconnect the observer once the element is found
-                observer.disconnect();
-                // Call the callback function with the target element
-                callback(document.querySelector(selector));
-            }
-        });
+    const observer = new MutationObserver(() => {
+        const elements = document.querySelectorAll(selector);
+        if (elements.length > 0) {
+            observer.disconnect();
+            callback(elements);
+        }
     });
-
-    // Start observing the document body and its subtree for changes
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
+    observer.observe(document.documentElement, { childList: true, subtree: true });
 }
